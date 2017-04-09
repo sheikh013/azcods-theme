05b27297277c7cbeba63f73b5e059acb423ff7e4
Shekh Kamal <shekh@azcods.com>
2017-04-09 14:58:31 +0530

    Home Page Finish

--- assets/functions/portfolio-widget.php
+++ assets/functions/portfolio-widget.php
@@ -1,91 +1,37 @@
 <?php
-
 add_shortcode('portfolio_widget', 'shortcode_portfolio_widget');
-
 function shortcode_portfolio_widget($atts, $content = null) {
     extract( shortcode_atts( array(
 		  'variable_1'	=> '',
       'variable_2'	=> '',
       'variable_3'	=> '',
-
 	), $atts ) );
     ob_start();
 ?>
-
 <div id="skhGrid">
     <ul class="grid cs-style-1">
+			<?php
+			$the_query = new WP_Query( array
+			( 'post_type' => 'project', 'posts_per_page' => 99 ) );
+			?>
+		    <?php while ( $the_query->have_posts() ):$the_query->the_post(); ?>
 				<li>
 					<figure>
-						<img src="http://azcods-v2.dev/wp-content/uploads/revslider/web-product-light/express_macbook_content1.jpg" alt="img01">
+						<?php $get_img = get_field('thumbnail_project');?>
+						<img src="<?php echo $get_img['url']; ?>" alt="<?php echo $get_img['alt']; ?>">
 						<figcaption>
-							<h3>Camera</h3>
+							<h3><?php the_title();?></h3>
 							<span></span>
-							<a href="http://dribbble.com/shots/1115632-Camera">Take a look</a>
+							<a href="<?php the_field('url_project');?>" target="_blank">Take a look</a>
 						</figcaption>
 					</figure>
 				</li>
-				<li>
-					<figure>
-						<img src="http://azcods-v2.dev/wp-content/uploads/revslider/web-product-light/express_macbook_content1.jpg" alt="img02">
-						<figcaption>
-							<h3>Music</h3>
-							<span></span>
-							<a href="http://dribbble.com/shots/1115960-Music">Take a look</a>
-						</figcaption>
-					</figure>
-				</li>
-				<li>
-					<figure>
-						<img src="http://azcods-v2.dev/wp-content/uploads/revslider/web-product-light/express_macbook_content1.jpg" alt="img04">
-						<figcaption>
-							<h3>Settings</h3>
-							<span></span>
-							<a href="http://dribbble.com/shots/1116685-Settings">Take a look</a>
-						</figcaption>
-					</figure>
-				</li>
-				<li>
-					<figure>
-						<img src="http://azcods-v2.dev/wp-content/uploads/revslider/web-product-light/express_macbook_content1.jpg" alt="img05">
-						<figcaption>
-							<h3>Safari</h3>
-							<span></span>
-							<a href="http://dribbble.com/shots/1116775-Safari">Take a look</a>
-						</figcaption>
-					</figure>
-				</li>
-				<li>
-					<figure>
-						<img src="http://azcods-v2.dev/wp-content/uploads/revslider/web-product-light/express_macbook_content1.jpg" alt="img03">
-						<figcaption>
-							<h3>Phone</h3>
-							<span></span>
-							<a href="http://dribbble.com/shots/1117308-Phone">Take a look</a>
-						</figcaption>
-					</figure>
-				</li>
-				<li>
-					<figure>
-						<img src="http://azcods-v2.dev/wp-content/uploads/revslider/web-product-light/express_macbook_content1.jpg" alt="img06">
-						<figcaption>
-							<h3>Game Center</h3>
-							<span></span>
-							<a href="http://dribbble.com/shots/1118904-Game-Center">Take a look</a>
-						</figcaption>
-					</figure>
-				</li>
-			</ul>
+			<?php endwhile; ?>
+	</ul>
 </div>
-
-
-
-
-
 <?php
-
     $content_data = ob_get_contents();
     ob_end_clean();
     return $content_data;
 }
-
 /** [portfolio_widget][/portfolio_widget]  **/
